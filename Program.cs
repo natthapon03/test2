5cb2e18766e38c70820e47a223581e21eabd4643
natthapon03 <pinnatthapon@gmail.com>
2023-04-03 10:36:43 +0700

    aster

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program{
+    static void Main(string[] args){
+
+        
+        Console.Write("Input number ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("pleas Input (end)");
+         int end = int.Parse(Console.ReadLine());
+       
+
+        Console.Write("pleas Input (FindMax, FindMin, FindMean)");
+         int  FindMax = int.Parse(Console.ReadLine());
+         int  FindMin = int.Parse(Console.ReadLine());
+         int  FindMean = int.Parse(Console.ReadLine());
+
+
+
+                
+
+        
+    }
+}
